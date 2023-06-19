69bed02d1d82a70b8c6ba81f21b815d9c344893d
CCR5 <matt-gomes@live.com>
2023-06-19 09:35:42 -0300

    First Commit

---
+++ internal/market/entity/investor.go
@@ -0,0 +1,49 @@
+package entity
+
+type Investor struct {
+	ID            string
+	Name          string
+	AssetPosition []*InvestorAssetPosition
+}
+
+func NewInvestor(id string) *Investor {
+	return &Investor{
+		ID:            id,
+		AssetPosition: []*InvestorAssetPosition{},
+	}
+}
+
+func (i *Investor) AddAssetPosition(assetPosition *InvestorAssetPosition) {
+	i.AssetPosition = append(i.AssetPosition, assetPosition)
+}
+
+func (i *Investor) UpdateAssetPosition(assetId string, qtdShares int) {
+	assetPosition := i.GetAssetPosition(assetId)
+	if assetPosition == nil {
+		i.AssetPosition = append(i.AssetPosition, NewInvestorAssetPosition(assetId, qtdShares))
+	} else {
+		assetPosition.Shares += qtdShares
+	}
+}
+
+func (i *Investor) GetAssetPosition(assetId string) *InvestorAssetPosition {
+	for _, assetPosition := range i.AssetPosition {
+		if assetPosition.AssetID == assetId {
+			return assetPosition
+		}
+	}
+
+	return nil
+}
+
+type InvestorAssetPosition struct {
+	AssetID string
+	Shares  int
+}
+
+func NewInvestorAssetPosition(assetId string, shares int) *InvestorAssetPosition {
+	return &InvestorAssetPosition{
+		AssetID: assetId,
+		Shares:  shares,
+	}
+}
